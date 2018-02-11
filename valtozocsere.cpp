050a75efe67977d35249ba552becbd68fcf3906f
baczoninorbert <36379913+baczoninorbert@users.noreply.github.com>
2018-02-11 23:19:34 +0100

    Add files via upload

---
+++ valtozocsere.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"eredetileg a=4 es b=7 \n";
+	int a = 4, b = 7;
+	//MEGOLDAS 1
+
+	a ^= b;
+	b ^= a;
+	a ^= b;
+	
+	cout<<"a: "<<a<<" b: "<<b<<endl;
+
+	a ^= b;
+	b ^= a;
+	a ^= b;
+
+	cout<<"a: "<<a<<" b: "<<b<<endl;
+	//MEGOLDAS 2
+
+	a=b-a;
+	b=b-a;
+	a=b+a;
+
+
+ 	cout<<"a: "<<a<<" b: "<<b<<endl;
+
+ 	a=b-a;
+ 	b=b-a;
+ 	a=b+a;
+
+  
+	cout<<"a: "<<a<<" b: "<<b<<endl;
+	
+	
+}
