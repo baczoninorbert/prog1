80c4678756ee98bf67999bd5c25670546b04b82b
baczoninorbert <36379913+baczoninorbert@users.noreply.github.com>
2018-05-09 02:33:49 +0200

    Create valtozo.py

---
+++ valtozocsere/valtozo.py
@@ -0,0 +1,26 @@
+a = 5
+b = 4
+print"a = ", a, " b = ", b
+print"Csere osszeadassal"
+a = a + b
+b = a - b
+a = a - b
+print"Lassuk: a = ", a, " b = ", b
+print"Uj ertekeket adunk"
+a = 2
+b = 3
+print"a = ", a, " b = ", b
+print"Csere szorzassal"
+a = a * b
+b = a / b
+a = a / b
+print"Lassuk: a = ", a, " b = ", b
+print"Uj ertekeket adunk"
+a = 6
+b = 7
+print"a = ", a, " b = ", b
+print"Csere exorral"
+a = b ^ a
+b = b ^ a
+a = b ^ a
+print "Lassuk: a = ", a, " b = ", b
